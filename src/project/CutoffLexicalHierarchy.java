f8cf17c079c2e064a81d1a0ac575dc1e1536bd61
Toberumono <JoshuaLipstone@gmail.com>
2016-05-24 17:37:49 -0500

    Updated model to store counts.

--- src/project/CutoffLexicalHierarchy.java
+++ src/project/CutoffLexicalHierarchy.java
@@ -3,6 +3,8 @@ package project;
 import java.io.IOException;
 import java.nio.file.Path;
 
+import edu.stanford.nlp.trees.TypedDependency;
+
 public class CutoffLexicalHierarchy extends Model {
 
 	public CutoffLexicalHierarchy(boolean requireSynchronized) {
@@ -16,6 +18,13 @@ public class CutoffLexicalHierarchy extends Model {
 	@Override
 	protected void smooth() {
 		
+	}
+
+	@Override
+	public Double probabilityForBigram(TypedDependency td) {
+		stem(td);
+		String name = td.reln().getLongName(), gov = generateKey(td.gov()), dep = generateKey(td.dep()), key = gov + " ~ " + dep;
+		return probs.get(td.reln().getLongName()).get(key);
 	}
 	
 }
--- src/project/ModelEvaluator.java
+++ src/project/ModelEvaluator.java
@@ -15,11 +15,11 @@ public class ModelEvaluator {
 	Pair<List<Pair<String, String>>, List<Double>> humanVNSeen;
 	Pair<List<Pair<String, String>>, List<Double>> humanVNUnseen;
 	
-	void digestTestData(Path directory) {
+	public void digestTestData(Path directory) {
 		
 	}
 	
-	Pair<List<Pair<String, String>>, List<Double>> digestTestData(Stream<String> pairs, Stream<String> means) {
+	public Pair<List<Pair<String, String>>, List<Double>> digestTestData(Stream<String> pairs, Stream<String> means) {
 		Stream<Pair<String, String>> paired = pairs.map(s -> s.split("\\s+")).map(s -> new Pair<>(s[0], s[1]));
 		Stream<Double> meaned = means.map(s -> s.split("\\s+")).map(s -> s[3]).map(Double::parseDouble);
 		return new Pair<>(paired.collect(Collectors.toList()), meaned.collect(Collectors.toList()));
