6d34cc1f7d115f7586b00a142ddc11619d127729
Toberumono <JoshuaLipstone@gmail.com>
2016-05-24 21:28:24 -0500

    Quick cleanup

--- src/project/CutoffLexicalHierarchy.java
+++ src/project/CutoffLexicalHierarchy.java
@@ -6,21 +6,6 @@ import java.nio.file.Path;
 import edu.stanford.nlp.trees.TypedDependency;
 
 public class CutoffLexicalHierarchy extends Model {
-	public DigestWornet(Path file) {
-		
-	}
-	
-	NounSynset nounSynset; 
-	NounSynset[] hyponyms; 
-
-	WordNetDatabase database = WordNetDatabase.getFileInstance(); 
-	Synset[] synsets = database.getSynsets("fly", SynsetType.NOUN); 
-	for (int i = 0; i < synsets.length; i++) { 
-	    nounSynset = (NounSynset)(synsets[i]); 
-	    hyponyms = nounSynset.getHyponyms(); 
-	    System.err.println(nounSynset.getWordForms()[0] + 
-	            ": " + nounSynset.getDefinition() + ") has " + hyponyms.length + " hyponyms"); 
-	}
 
 	public CutoffLexicalHierarchy(boolean requireSynchronized) {
 		super(requireSynchronized);
